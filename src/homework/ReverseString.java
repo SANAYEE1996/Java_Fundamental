09164c3e58a2e43a6c4bde17ee8589191043a888
SANAYEE1996 <dudtkd0219@gmail.com>
2021-05-12 17:52:45 +0900

    two type reverse code

--- src/homework/ReverseString.java
+++ src/homework/ReverseString.java
@@ -8,9 +8,18 @@ public class ReverseString {
 		}
 		return s;
 	}
+	
+	public static String reve(String str) {
+		String s = "";
+		for(int i = str.length(); i > 0; i--) {
+			s = s.concat(str.substring(i-1,i));
+		}	
+		return s;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(reverse("it is not your fault!!"));
+		System.out.println(reve("it is not your fault!!"));
 	}
 
 }
